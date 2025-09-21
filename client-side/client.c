ad976480e858e715d72d749c0bbb180e35157751
swayam614 <swayampalrecha6@gmail.com>
2025-09-21 17:14:53 +0530

    Make Function of sending request from client side

--- client-side/client.c
+++ client-side/client.c
@@ -9,6 +9,10 @@
 
 // following will go in header file client.h
 #define TCP_CONNECTION_CODE_ERROR_EXIT_CODE 55
+#define TCP_RW_BUFFER_SIZE 16384
+#define TCP_RW_HEADER_SIZE 4
+#define TCP_SEND_LIMIT 4294967295 - TCP_RW_HEADER_SIZE
+
 struct _tcp_connection; // will be defined in client.c
 typedef struct _tcp_connection tcp_connection;
 
@@ -18,6 +22,8 @@ void tcp_connection_error(tcp_connection *, char **error_str);
 void disconnect_tcp_connection(tcp_connection *);
 void release_tcp_connection(tcp_connection *);
 
+void tcp_connection_send(tcp_connection *, const char *, uint32_t);
+char *tcp_connection_receive(tcp_connection *, uint32_t *);
 // following will go in client.c
 
 typedef struct _tcp_connection
@@ -109,13 +115,25 @@ int tcp_connection_failed(tcp_connection *connection)
 
 void tcp_connection_error(tcp_connection *connection, char **error_str)
 {
+    char *error800 = "Connection not established";
     char *error801 = "Invalid Port Number";
     char *error802 = "Unable to connect";
+    char *error803 = "Connection is closed or not yet established";
+    char *error804 = "No data to send";
+    char *error805 = "Cannot send more than 4294967291 bytes in one go";
+    char *error806 = "Low memory";
 
-    if (connection == NULL || error_str == NULL)
+    if (error_str == NULL)
         return;
 
-    if (connection->error_type == 'G')
+    if (connection == NULL)
+    {
+        *error_str = (char *)malloc(sizeof(char) * (strlen(error800) + 1));
+        if (*error_str != NULL)
+            strcpy(*error_str, error800);
+    }
+
+    else if (connection->error_type == 'G')
     {
         *error_str = (char *)malloc(sizeof(char) * (strlen(gai_strerror(connection->error_number)) + 1));
         if (*error_str != NULL)
@@ -140,6 +158,30 @@ void tcp_connection_error(tcp_connection *connection, char **error_str)
             *error_str = (char *)malloc(sizeof(char) * (strlen(error802) + 1));
             if (*error_str != NULL)
                 strcpy(*error_str, error802);
+        }
+        else if (connection->error_number == 803)
+        {
+            *error_str = (char *)malloc(sizeof(char) * (strlen(error803) + 1));
+            if (*error_str != NULL)
+                strcpy(*error_str, error803);
+        }
+        else if (connection->error_number == 804)
+        {
+            *error_str = (char *)malloc(sizeof(char) * (strlen(error804) + 1));
+            if (*error_str != NULL)
+                strcpy(*error_str, error804);
+        }
+        else if (connection->error_number == 805)
+        {
+            *error_str = (char *)malloc(sizeof(char) * (strlen(error805) + 1));
+            if (*error_str != NULL)
+                strcpy(*error_str, error805);
+        }
+        else if (connection->error_number == 806)
+        {
+            *error_str = (char *)malloc(sizeof(char) * (strlen(error806) + 1));
+            if (*error_str != NULL)
+                strcpy(*error_str, error806);
         }
         else
         {
@@ -171,12 +213,129 @@ void release_tcp_connection(tcp_connection *connection)
     free(connection);
 }
 
+void tcp_connection_send(tcp_connection *connection, const char *buffer, uint32_t buffer_size)
+{
+    uint32_t our_buffer_size;
+    uint32_t our_buffer_index;
+    uint32_t buffer_index;
+    char *our_buffer;
+    uint32_t pick_in_this_cycle;
+    uint32_t number_of_bytes_to_send;
+    ssize_t bytes_sent;
+    uint32_t network_byte_order;
+
+    if (connection == NULL)
+    {
+        return;
+    }
+    if (connection->socket_descriptor_closed == 1)
+    {
+        connection->error_number = 803; // connection is closed or not yet established
+        connection->error_type = 'C';
+        return;
+    }
+    if (buffer == NULL || buffer_size == 0)
+    {
+        connection->error_number = 804; // No data to send
+        connection->error_type = 'C';
+        return;
+    }
+    if (buffer_size > TCP_SEND_LIMIT)
+    {
+        connection->error_number = 805; // Cannot send more than 4294967291 bytes in one go
+        connection->error_type = 'C';
+        return;
+    }
+
+    if (buffer_size + TCP_RW_HEADER_SIZE <= TCP_RW_BUFFER_SIZE)
+    {
+        our_buffer_size = buffer_size + TCP_RW_HEADER_SIZE;
+    }
+    else
+    {
+        our_buffer_size = TCP_RW_BUFFER_SIZE;
+    }
+
+    our_buffer = (char *)malloc(our_buffer_size);
+    if (our_buffer == NULL)
+    {
+        connection->error_number = 806; // low memory
+        connection->error_type = 'C';
+        return;
+    }
+
+    network_byte_order = htonl(buffer_size);
+    memcpy(our_buffer, &network_byte_order, sizeof(uint32_t));
+
+    number_of_bytes_to_send = buffer_size + TCP_RW_HEADER_SIZE;
+    our_buffer_index = TCP_RW_HEADER_SIZE; // 0,1,2 and 3 are used for header
+    buffer_index = 0;
+
+    while (number_of_bytes_to_send > 0)
+    {
+        if (number_of_bytes_to_send > (our_buffer_size - TCP_RW_HEADER_SIZE))
+        {
+            pick_in_this_cycle = our_buffer_size - our_buffer_index;
+        }
+        else
+        {
+            pick_in_this_cycle = number_of_bytes_to_send - our_buffer_index;
+        }
+        memcpy(our_buffer + our_buffer_index, buffer + buffer_index, pick_in_this_cycle);
+        bytes_sent = send(connection->socket_descriptor, our_buffer, our_buffer_index + pick_in_this_cycle, 0);
+        if (bytes_sent == -1)
+        {
+            // some issue, it can be because of some raised signal or some other serious issue
+            // we will abort the send process , we dont retry
+            connection->error_number = errno;
+            connection->error_type = 'G';
+            free(our_buffer);
+            return;
+        }
+        buffer_index = buffer_index + bytes_sent - our_buffer_index;
+        our_buffer_index = 0;
+        number_of_bytes_to_send = number_of_bytes_to_send - bytes_sent;
+    } // loop to send data chunks , ends here
+
+    free(our_buffer);
+}
+char *tcp_connection_receive(tcp_connection *connection, uint32_t *buffer_size)
+{
+    return NULL;
+}
+
 // following is the code to test the client implementation of network layer
 
 int main()
 {
     char *error_str;
+    char data[500000];
+    int i;
+    char *response;
+    uint32_t response_size;
+
     tcp_connection *connection = tcp_connect("localhost", 5046);
+    if (tcp_connection_failed(connection))
+    {
+        tcp_connection_error(connection, &error_str);
+        if (error_str != NULL)
+        {
+            printf("%s\n", error_str);
+            free(error_str);
+        }
+        disconnect_tcp_connection(connection); // if you want this line can be removed
+        release_tcp_connection(connection);
+        exit(TCP_CONNECTION_CODE_ERROR_EXIT_CODE);
+    }
+
+    // code to send request data
+
+    for (i = 0; i < 500000; i++)
+    {
+        data[i] = (char)((i % 10) + 48);
+    }
+
+    tcp_connection_send(connection, data, 500000);
     if (tcp_connection_failed(connection))
     {
         tcp_connection_error(connection, &error_str);
@@ -190,7 +349,34 @@ int main()
         exit(TCP_CONNECTION_CODE_ERROR_EXIT_CODE);
     }
 
-    // code to send / revice data
+    // code to send reuqest data ends here
+
+    // code to receive response data starts here
+
+    response = tcp_connection_receive(connection, &response_size);
+    if (tcp_connection_failed(connection))
+    {
+        tcp_connection_error(connection, &error_str);
+        if (error_str != NULL)
+        {
+            printf("%s\n", error_str);
+            free(error_str);
+        }
+        disconnect_tcp_connection(connection);
+        release_tcp_connection(connection);
+        exit(TCP_CONNECTION_CODE_ERROR_EXIT_CODE);
+    }
+
+    printf("Number of bytes received in response %u\n", response_size);
+    for (i = 0; i < response_size; i++)
+    {
+        printf("%c", response[i]);
+    }
+    printf("\n");   // to ensure that all bytes get printed as \n ensures that the
+                    // contents of internl buffer are flushed to output stream (stdout)
+    free(response); // this has to be well documented
+    // code to receive response data ends here
+
     disconnect_tcp_connection(connection);
     release_tcp_connection(connection);
     return 0;
